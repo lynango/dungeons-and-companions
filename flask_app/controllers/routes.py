6d353661c0a2811fec03c019821f1e47451c3a00
lynango <lynango5623@gmail.com>
2022-05-18 19:36:22 -0700

    first commit

---
+++ flask_app/controllers/routes.py
@@ -0,0 +1,57 @@
+from flask import render_template,redirect,session,request, flash
+from flask_app import app
+from flask_app.models.user import User
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
+
+#Shows the login and registration page
+@app.route('/')
+def index():
+    return render_template('login_and_registration.html')
+
+#Process user's request to register
+@app.route('/register',methods=['POST'])
+def register():
+    if not User.registration(request.form):
+        return redirect('/')
+    data ={ 
+        "first_name": request.form['first_name'],
+        "last_name": request.form['last_name'],
+        "email": request.form['email'],
+        "password": bcrypt.generate_password_hash(request.form['password'])
+    }
+    user_id = User.save(data)
+    session['user_id'] = user_id
+    return redirect('/welcome')
+
+#Process user's request to login
+@app.route('/login',methods=['POST'])
+def login():
+    data = {"email": request.form['email']} 
+    user_with_email = User.get_by_email(data) 
+    if user_with_email == False:
+        flash("Invalid Email/Password","login") 
+        return redirect('/')
+    if not bcrypt.check_password_hash(user_with_email.password, request.form['password']): 
+        flash("Invalid Email/Password","login") 
+        return redirect('/')
+    session['user_id'] = user_with_email.id
+    return redirect('/welcome') 
+
+#Validation checkpoint for logged in users
+@app.route('/welcome')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect('/logout')  # ->Checks to see if the user had login or not. If not, the user is redirected to the front page.
+    data ={
+        'id': session['user_id']    # ->If the user had login, the user is directed to the home page
+    }
+    one_user = User.get_one(data)                                                                                                                                              
+    return render_template("welcome_page.html", current_user = one_user)
+
+#Process user's request to logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask_app import app
 
-# from flask_app.controllers import " "  #import the controller files here
+#import the controller files here
+from flask_app.controllers import routes 
 
 if __name__ == "__main__":
     app.run(debug=True)
