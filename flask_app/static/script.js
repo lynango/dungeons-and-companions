63a2ca48300e467708387e58c539c33ff1f3d9fb
Taylor Burrows <burrows.taylor@gmail.com>
2022-05-26 00:29:14 -0400

    minor updates
    create character and update character pages have been formatted

---
+++ flask_app/static/script.js
@@ -0,0 +1,9 @@
+function over(element){
+    element.style.backgroundColor= "black";
+    element.style.color = "rgb(255,255,255)";
+}
+
+function out(element){
+    element.style.backgroundColor = "rgb(255,255,255)";
+    element.style.color = "black";
+}
